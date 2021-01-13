9071c939ed3388331f9fa23e5359b4b282682345
Tabitha Wong <tabi159w@gmail.com>
2021-01-12 22:17:41 -0500

    on scroll header

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ var sticky = header.offsetTop;
 function myFunction() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
-  } else {
+  } 
+  else {
     header.classList.remove("sticky");
   }
 }
